c661a66f092479558e62e97ffc38979a32326f7b
XilianRose <m.stegeman@me.com>
2023-07-28 15:05:32 +0200

    char to binary converison added

--- src/client.c
+++ src/client.c
@@ -6,14 +6,60 @@
 /*   By: mstegema <mstegema@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/27 14:05:57 by mstegema      #+#    #+#                 */
-/*   Updated: 2023/07/27 14:17:20 by mstegema      ########   odam.nl         */
+/*   Updated: 2023/07/28 14:25:07 by mstegema      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minitalk.h"
 
-int	main(void)
+char	*char_to_binary(char c)
 {
-	ft_printf("client\n");
+	char	*res;
+	int		ascii;
+	int		i;
+
+	res = ft_calloc(8 + 1, sizeof(char));
+	if (!res)
+		exit (1);
+	ascii = (int)c;
+	i = 7;
+	while (ascii > 0)
+	{
+		if (ascii % 2 == 1)
+			res[i] = '1';
+		else
+			res[i] = '0';
+		ascii = ascii / 2;
+		i--;
+	}
+	while (i >= 0)
+	{
+		res[i] = '0';
+		i--;
+	}
+	return (res);
+}
+
+int	send_signals(char *binary_c)
+{
+
+}
+
+int	main(int argc, char **argv)
+{
+	char	*str;
+	int		len;
+	int		i;
+
+	if (argc != 3)
+		exit(1);
+	str = argv[2];
+	len = ft_strlen(str);
+	i = 0;
+	while (i < len)
+	{
+		ft_printf("%s ", char_to_binary(str[i]));
+		i++;
+	}
 	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -6,17 +6,25 @@
 /*   By: mstegema <mstegema@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/27 14:05:36 by mstegema      #+#    #+#                 */
-/*   Updated: 2023/07/27 14:17:27 by mstegema      ########   odam.nl         */
+/*   Updated: 2023/07/28 14:51:41 by mstegema      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minitalk.h"
 
+void	handle_signal(int sig)
+{
+
+}
+
 int	main(void)
 {
-	int	pid;
+	struct sigaction	sa;
+	int					pid;
 
-	pid = (int)getpid();
+	pid = getpid();
+	sa.sa_handler = &handle_signal;
 	ft_printf("%i\n", pid);
+	pause();
 	return (0);
 }
