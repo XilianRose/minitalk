8336ebc5003a7f8d9a2a4e8bb539a5b8639ea6a6
XilianRose <m.stegeman@me.com>
2023-07-28 15:10:13 +0200

    started signal handling

--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: mstegema <mstegema@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/27 14:05:36 by mstegema      #+#    #+#                 */
-/*   Updated: 2023/07/28 14:51:41 by mstegema      ########   odam.nl         */
+/*   Updated: 2023/07/28 15:09:48 by mstegema      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@ int	main(void)
 
 	pid = getpid();
 	sa.sa_handler = &handle_signal;
+	// sa.sa_flags = SA
+	sigaction(SIGUSR1, &sa, NULL);
 	ft_printf("%i\n", pid);
 	pause();
 	return (0);
